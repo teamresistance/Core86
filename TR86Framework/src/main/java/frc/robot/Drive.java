d3500aaa683b6f06282e63fb519efed191db48ea
Kinfe <kinfebankole@gmail.com>
2019-10-19 23:13:18 -0400

    -will update with code for Drive, subsystem soon -will add XboxControl and auto and vision later -need to update README

---
+++ TR86Framework/src/main/java/frc/robot/Drive.java
@@ -0,0 +1,8 @@
+//The Drive class is a special subsystem because it controls how the robot itself moves
+//The class usually contains some kind of PID to make driving more precise
+
+package frc.robot;
+
+public class Drive {
+
+}
---
+++ TR86Framework/src/main/java/frc/robot/Subsystem.java
@@ -0,0 +1,8 @@
+//Subsystems are the seperate files that program seperate parts for the bot
+//A subsystem usually contains some kind of state machine to make certain actions happen at certain times (i.e. button press)
+
+package frc.robot;
+
+public class Subsystem {
+    
+}
